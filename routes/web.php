e67b6fc6906c2a67037419af571f0757a24d70a9
Rungruoi <rungruoi99it@gmail.com>
2021-09-12 16:46:29 +0700

    init business core

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/admin', function () {
+    return view('admin.index');
+});
+
+Route::get('password/reset', function () {
+    return view('auth.passwords.email');
+})->name('password.reset');
+
+Route::get('/profile', function () {
+    return view('auth.profile');
+})->name('profile.edit');
+
+Route::get('/change-password', function () {
+    return view('auth.passwords.change');
+})->name('auth.password.edit');
+
+Route::get('/login', function () {
+    return view('auth.login');
+});
