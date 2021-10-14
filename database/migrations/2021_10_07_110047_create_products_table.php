8ed55836052eee4e3c00bd914a309d2f020eeadc
Rungruoi <48022090+Rungruoi@users.noreply.github.com>
2021-10-14 15:13:50 +0700

    fix screen create product (#13)

--- database/migrations/2021_10_07_110047_create_products_table.php
+++ database/migrations/2021_10_07_110047_create_products_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreateProductsTable extends Migration
@@ -25,7 +26,8 @@ class CreateProductsTable extends Migration
             $table->integer('date_sell');
             $table->tinyInteger('status')->default(1)
                 ->comment('1 => active, 2 => inactive');
-            $table->timestamps();
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
         });
     }
 
